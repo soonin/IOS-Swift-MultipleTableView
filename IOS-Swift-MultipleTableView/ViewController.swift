a604549fbc66501573a90aa8cc6ed14c8934afec
Pooya Hatami <am.hatami@gmail.com>
2018-11-12 01:24:34 -0800

    update!

--- IOS-Swift-MultipleTableView/ViewController.swift
+++ IOS-Swift-MultipleTableView/ViewController.swift
@@ -8,13 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
 
+    @IBOutlet weak var topTableView: UITableView!
+    @IBOutlet weak var downTableview: UITableView!
+    var topData : [String] = []
+    var downData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topTableView.delegate = self
+        downTableview.delegate = self
+        topTableView.dataSource = self
+        downTableview.dataSource = self
+        
+        for index in 0...20 {
+            topData.append("Top Table Row \(index)")
+        }
+        
+        for index in 10...45 {
+            downData.append("Down Table Row \(index)")
+        }
+
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        var numberOfRow = 1
+        switch tableView {
+        case topTableView:
+            numberOfRow = topData.count
+        case downTableview:
+            numberOfRow = downData.count
+        default:
+            print("Some things Wrong!!")
+        }
+        return numberOfRow
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        switch tableView {
+        case topTableView:
+            cell = tableView.dequeueReusableCell(withIdentifier: "topCell", for: indexPath)
+            cell.textLabel?.text = topData[indexPath.row]
+            cell.backgroundColor = UIColor.green
+        case downTableview:
+            cell = tableView.dequeueReusableCell(withIdentifier: "downCell", for: indexPath)
+            cell.textLabel?.text = downData[indexPath.row]
+            cell.backgroundColor = UIColor.yellow
+        default:
+            print("Some things Wrong!!")
+        }
+        return cell
+    }
+    
+    
 
 }
 
