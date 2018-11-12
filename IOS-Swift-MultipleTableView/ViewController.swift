dfcc44f1e8e51691b456f7c999747709f1bbd12c
Pooya Hatami <am.hatami@gmail.com>
2018-11-12 01:06:28 -0800

    Initial Commit

---
+++ IOS-Swift-MultipleTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-Swift-MultipleTableView
+//
+//  Created by Pooya on 2018-11-12.
+//  Copyright © 2018 Pooya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
